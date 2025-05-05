4bbc5c0d80d131cdb9b913db5d3385bfdabc98d8
RahafQahtani <irahafque@gmail.com>
2025-05-05 12:28:22 +0300

    Remove <filename>

--- SPI/spi/sv/spi_master_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class spi_master_sequencer extends uvm_sequencer #(spi_transaction);
-  `uvm_component_utils(spi_master_sequencer)
- 
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
--- SPI/spi/sv/spi_slave_sequencer.sv
+++
@@ -1,7 +0,0 @@
-class spi_slave_sequencer extends uvm_sequencer#(spi_transaction);
-  `uvm_component_utils(spi_slave_sequencer)
-
-  function new(string name = "spi_slave_sequencer",uvm_component parent);
-    super.new(name,parent);
-  endfunction
-endclass
